e3b547ffc4a0ec8359c9f0d900dd945cca4fb328
Doug Enas <douglassenas@gmail.com>
2019-07-02 10:40:17 -0400

    updated pipenv from 2.7 to 3.7

--- djropbox/urls.py
+++ djropbox/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from djropbox.views import homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homepage)
 ]
